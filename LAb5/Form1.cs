8d465ba3cd86415e97954d20e208902701205792
TeamLead C# <bazarovivan7@gmail.com>
2023-11-11 19:59:44 +0200

    Add project files.

---
+++ LAb5/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LAb5
+{
+    public partial class Form1 : Form
+    {
+        private SeriesCalculator seriesCalculator = new SeriesCalculator();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double start = Convert.ToDouble(textBox1.Text);
+                double end = Convert.ToDouble(textBox2.Text);
+                double step = Convert.ToDouble(textBox3.Text);
+
+                listBox1.Items.Clear();
+
+                for (double x = start; x <= end; x += step)
+                {
+                    listBox1.Items.Add($"Цикл: x = {x}, значення ряду: {seriesCalculator.CalculateWithLoop(start, end)}");
+                    listBox1.Items.Add($"Рекурсія: x = {x}, значення ряду: {seriesCalculator.CalculateWithRecursion(start, end)}");
+                }
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Будь ласка, введіть коректні дані");
+            }
+        }
+    }
+}
---
+++ LAb5/SeriesCalculator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAb5
+{
+    public class SeriesCalculator
+    {
+        public double CalculateWithLoop(double x, double N)
+        {
+            double result = 1;
+            double factorial = 1;
+            for (int i = 1; i <= N; i++)
+            {
+                factorial *= i;
+                result += Math.Pow(x, i) / factorial;
+            }
+            return result;
+        }
+
+        public double CalculateWithRecursion(double x, double N)
+        {
+            return CalculateRecursively(x, N, 1, 1, 1);
+        }
+
+        private double CalculateRecursively(double x, double N, int i, double factorial, double result)
+        {
+            if (i > N)
+            {
+                return result;
+            }
+            factorial *= i;
+            result += Math.Pow(x, i) / factorial;
+            return CalculateRecursively(x, N, i + 1, factorial, result);
+        }
+    }
+}
